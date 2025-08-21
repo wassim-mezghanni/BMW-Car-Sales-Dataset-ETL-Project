c9a761f5e761201b65ec24ca3c6a062e09217b16
wass16 <wassim.wmez@gmail.com>
2025-08-21 16:25:53 +0100

    Average selling price by BMW model

--- scripts/etl_pipeline.py
+++ scripts/etl_pipeline.py
@@ -11,6 +11,7 @@ df.dropna(inplace=True)
 df['Year'] = df['Year'].astype(int)  
 
 df.to_csv("data/processed/cleaned_BMW_Car_Sales_Classification.csv", index=False)
+
 # ----- Load -----
 
 ## 1. Connect to Postgres
@@ -34,6 +35,22 @@ print(df_sales)
 
 df_sales.plot(kind="bar", x="Year", y="total_sales", legend=False, title="Total Sales per Year")
 plt.savefig("data/processed/sales_per_year.png")
+plt.close()
+
+#  Average selling price by model
+query2 = """
+SELECT "Model", AVG("Price_USD") AS avg_price
+FROM bmw_car_sales
+GROUP BY "Model"
+ORDER BY avg_price DESC;
+"""
+df_price = pd.read_sql(query2, engine)
+print("\n Average Selling Price by Model:")
+print(df_price.head(10))
+
+# Plot top 10
+df_price.head(10).plot(kind="barh", x="Model", y="avg_price", legend=False, title="Top 10 Models by Avg Price")
+plt.savefig("data/processed/avg_price_by_model.png")
 plt.close()
 
 
