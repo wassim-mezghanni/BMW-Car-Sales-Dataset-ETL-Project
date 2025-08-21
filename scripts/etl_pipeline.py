fad780b44e81a092581e6869586c1026f2bd7a1c
wass16 <wassim.wmez@gmail.com>
2025-08-21 16:01:45 +0100

    total sales per year for BMW cars

--- scripts/etl_pipeline.py
+++ scripts/etl_pipeline.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 
 
 #  ----- Extract -----
-df = pd.read_csv("../data/raw/BMW_Car_Sales_Classification.csv")
+df = pd.read_csv("data/raw/BMW_Car_Sales_Classification.csv")
 
 #  ----- Transform -----
 df.dropna(inplace=True)  
 df['Year'] = df['Year'].astype(int)  
 
-df.to_csv("../data/processed/cleaned_BMW_Car_Sales_Classification.csv", index=False)
+df.to_csv("data/processed/cleaned_BMW_Car_Sales_Classification.csv", index=False)
 # ----- Load -----
 
 ## 1. Connect to Postgres
-engine = create_engine("postgresql://admin:admin@localhost:5432/BMW_Car_Sales_Classification=#")
+engine = create_engine("postgresql://admin:admin@localhost:5432/bmw_sales")
 df.to_sql('bmw_car_sales', engine, if_exists='replace', index=False)
 print(" Data loaded into PostgreSQL!")
  
@@ -23,7 +23,7 @@ print(" Data loaded into PostgreSQL!")
 # Total sales per year
 query1 = """
 SELECT "Year", COUNT(*) AS total_sales
-FROM BMW_Car_Sales_Classification
+FROM bmw_car_sales
 GROUP BY "Year"
 ORDER BY "Year";
 """
@@ -32,9 +32,8 @@ df_sales = pd.read_sql(query1, engine)
 print("\n Total Sales per Year:")
 print(df_sales)
 
-df_sales.plot(kind ="")
-df_sales.plot(kind="bar", x="Year", y="total_sales", legend=true, title="Total Sales per Year")
-plt.savefig("../data/processed/sales_per_year.png")
+df_sales.plot(kind="bar", x="Year", y="total_sales", legend=False, title="Total Sales per Year")
+plt.savefig("data/processed/sales_per_year.png")
 plt.close()
 
 
